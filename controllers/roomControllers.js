01a53a58115a8a2b4fcabb212e2c42fe5b5dd83e
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2021-07-14 18:52:16 -0400

    (feat: modified get all rooms query to display all rooms stored in db)

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -1,13 +1,23 @@
 import Room from '../models/room';
 
-const allRooms = (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: 'Successfully retrieved all rooms',
-  });
+const allRooms = async (req, res) => {
+  try {
+    const rooms = await Room.find();
+
+    res.status(200).json({
+      success: true,
+      count: rooms.length,
+      rooms,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
 };
 
-//Create new room  => /api/rooms
+//*Create new room  => /api/rooms
 const newRoom = async (req, res) => {
   try {
     const room = await Room.create(req.body);
@@ -24,7 +34,4 @@ const newRoom = async (req, res) => {
   }
 };
 
-export {
-  allRooms,
-  newRoom,
-};
+export { allRooms, newRoom };
