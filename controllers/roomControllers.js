b65bbc313989103996cb691f5b28128e6fc6a191
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2021-07-15 19:50:03 -0400

    (feat: added search, filter and pagination to backend)

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -1,14 +1,26 @@
 import Room from '../models/room';
 import ErrorHandler from '../utils/errorHandler';
 import catchAsyncErrors from '../middlewares/catchAsyncErrors';
+import APIFeatures from '../utils/apiFeatures';
 
 //* Get all rooms => /api/rooms
 const allRooms = catchAsyncErrors(async (req, res) => {
-  const rooms = await Room.find();
+  const resPerPage = 4;
+  const roomsCount = await Room.countDocuments();
+
+  const apiFeatures = new APIFeatures(Room.find(), req.query).search().filter();
+
+  let rooms = await apiFeatures.query;
+  let filteredRoomsCount = rooms.length;
+
+  apiFeatures.pagination(resPerPage);
+  rooms = await apiFeatures.query;
 
   res.status(200).json({
     success: true,
-    count: rooms.length,
+    roomsCount,
+    resPerPage,
+    filteredRoomsCount,
     rooms,
   });
 });
