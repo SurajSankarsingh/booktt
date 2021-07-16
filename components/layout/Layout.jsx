5657aff0437f071a5e329f2027fa4b19af7b88bb
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2021-07-15 20:20:28 -0400

    (feat: added document.js, header, footer and layout components)

---
+++ components/layout/Layout.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Head from 'next/head';
+
+import Header from './Header';
+import Footer from './Footer';
+
+const Layout = ({ children, title = 'Rooms in Trinidad & Tobago' }) => {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
+
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
