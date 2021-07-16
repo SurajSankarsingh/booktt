ad6caf28960b6d421a98f96fbd6936cccac357ee
SurajSankarsingh <suraj.sankarsingh@hotmail.com>
2021-07-16 02:31:19 -0400

    (feat: set up redux store and next store, also added wrapper to app)

---
+++ redux/reducers/reducers.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+const reducer = combineReducers({
+    // Add reducers here
+});
+
+export default reducer;
---
+++ redux/store.js
@@ -0,0 +1,31 @@
+import { createStore, applyMiddleware } from 'redux';
+import { HYDRATE, createWrapper } from 'next-redux-wrapper';
+import ThunkMiddleware  from 'redux-thunk';
+import reducers from './reducers/reducers';
+
+const bindMiddleware = (middlewares) => {
+  if (process.env.NODE_ENV !== 'production') {
+    const { composeWithDevTools } = require('redux-devtools-extension');
+    return composeWithDevTools(applyMiddleware(...middlewares));
+  }
+
+  return applyMiddleware(...middlewares);
+};
+
+const reducer = (state, action) => {
+  if (action.type === HYDRATE) {
+    const nextState = {
+      ...state,
+      ...action.payload,
+    };
+    return nextState;
+  } else {
+    return reducers(state, action);
+  }
+};
+
+const initStore = () => {
+  return createStore(reducer, bindMiddleware(ThunkMiddleware));
+};
+
+export const wrapper = createWrapper(initStore);
